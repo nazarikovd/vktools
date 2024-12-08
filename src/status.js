ba86e047a72db51c66d9b0d6a40d526d16d22fd0
Даня Назариков <81469711+nazarikovd@users.noreply.github.com>
2024-12-09 02:13:47 +0300

    Add files via upload

---
+++ src/status.js
@@ -0,0 +1,23 @@
+let { API } = require('vk-io')
+
+module.exports = class VKStatus{
+
+	async process(text){
+		//to do
+	}
+
+	async set(text, token){
+
+		let vkapi = new API({
+		    token: token
+		});
+
+		let res = await vkapi.status.set({
+			"text": text
+		})
+
+		return true
+
+	}
+
+}
